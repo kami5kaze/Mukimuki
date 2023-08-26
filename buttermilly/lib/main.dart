dabfcbc499476d1b38f6c3d2751acacb79a70141
reiji-yasuda <gtu89384@kwansei.ac.jp>
2023-08-26 14:09:52 +0900

    ログイン機能の実装

--- buttermilly/lib/main.dart
+++ buttermilly/lib/main.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,18 +29,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -48,68 +40,160 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  
+  String _email = '';
+  String _password = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _tab = <Tab> [
+    Tab( text:'新規登録'),
+    Tab( text:'ログイン'),
+  ];
+  // Widget build(BuildContext context) {
+  //   return DefaultTabController(
+  //     length: _tab.length,
+  //     child: Scaffold(
+  //       appBar: AppBar(
+  //         title: const Text('TabBar'),
+  //         bottom: TabBar(
+  //           tabs: _tab,
+  //         ),
+  //       ),
+  //       body: TabBarView(
+  //           children: <Widget> [
+  //             TabPage(title: '新規登録'),
+  //             TabPage(title: 'ログイン'),
+  //           ]
+  //       ),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    return DefaultTabController(
+      length: _tab.length,
+      child: Scaffold(
+        appBar: AppBar(
+          
+          title: const Text('buttermily'),
+          bottom: TabBar(
+          tabs: _tab,
+          ),
+        ),
+        body: TabBarView(
+          children:  <Widget> [
+            _registerWidget(),
+            _loginWidget(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }  
+
+  Widget _registerWidget(){
+    return Center(
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // 1行目 メールアドレス入力用テキストフィールド
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'メールアドレス'),
+                      onChanged: (String value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                    // 2行目 パスワード入力用テキストフィールド
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'パスワード'),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('ユーザ登録'),
+                      onPressed: () async {
+                        try {
+                          final User? user = (await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: _email, password: _password))
+                              .user;
+                          if (user != null)
+                            print("ユーザ登録しました ${user.email} , ${user.uid}");
+                        } catch (e) {
+                          print("ユーザーネームは13文字以内で英数字とアンダーバー(_)のみでご記入ください");
+                        }
+                      },
+                      
+                    ),
+                    
+                  ],
+                ),
+              ),
+            );
+  }
+  Widget _loginWidget(){
+    return Center(
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // 1行目 メールアドレス入力用テキストフィールド
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'メールアドレス'),
+                      onChanged: (String value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                    // 2行目 パスワード入力用テキストフィールド
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'パスワード'),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('ログイン'),
+                      onPressed: () async {
+                        try {
+                          // メール/パスワードでログイン
+                          final User? user = (await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: _email, password: _password))
+                              .user;
+                          if (user != null)
+                            print("ログインに成功しました。 ${user.email} , ${user.uid}");
+                        } catch (e) {
+                          print("ログインに失敗しました");
+                        }
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('パスワードリセット'),
+                      onPressed: () async {
+                        try {
+                          await FirebaseAuth.instance
+                              .sendPasswordResetEmail(email: _email);
+                          print("パスワードリセット用のメールを送信しました");
+                        } catch (e) {
+                          print(e);
+                        }
+                    }),
+                  ],
+                ),
+              ),
+            );
   }
 }
