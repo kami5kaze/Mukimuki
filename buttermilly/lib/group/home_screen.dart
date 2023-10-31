a376bbe230042134cbf766a066970e75ac89b61d
[kusoerooyajikamiyama@gmail.com] <kusoerooyajikamiyama@gmail.com>
2023-10-31 15:10:35 +0900

    ホーム画面から投稿するボタン追加

--- buttermilly/lib/group/home_screen.dart
+++ buttermilly/lib/group/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 import 'package:buttermilly/account/account.dart';
+import 'package:buttermilly/post_page/post_papge.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -14,7 +15,6 @@ class _Screen1State extends State<HomeScreen> {
   ImagePicker picker = ImagePicker();
   bool isFavorite = false;
   Future<void> getImageFromGallery() async {
-    
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
@@ -105,20 +105,20 @@ class _Screen1State extends State<HomeScreen> {
                     GestureDetector(
                       onDoubleTap: () {
                         setState(() {
-                                  isFavorite =! isFavorite; // 変数をトグルして色を切り替え
-                                          });
+                          isFavorite = !isFavorite; // 変数をトグルして色を切り替え
+                        });
                       },
                       child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Color.fromARGB(255, 163, 237, 225),
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    'image/fireflower.jpg',
-                                  ),
-                                  fit: BoxFit.cover ,
-                                ),
-                              ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromARGB(255, 163, 237, 225),
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'image/fireflower.jpg',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                         width: 280,
                         height: 390,
                       ),
@@ -139,14 +139,12 @@ class _Screen1State extends State<HomeScreen> {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Padding(
-                                    padding: const EdgeInsets.only(left: 15),
-                                    
-                                    child: IconButton(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: IconButton(
                                         icon: Icon(Icons.favorite,
                                             color: isFavorite
                                                 ? Colors.pink
-                                                : Colors.grey
-                                                ),
+                                                : Colors.grey),
                                         onPressed: () {
                                           setState(() {
                                             isFavorite =
@@ -154,8 +152,7 @@ class _Screen1State extends State<HomeScreen> {
                                           });
                                         },
                                         //タップ中の色
-                                    )
-                                  ),
+                                      )),
                                   Padding(
                                     padding: const EdgeInsets.only(left: 10),
                                     child: Container(
@@ -169,11 +166,9 @@ class _Screen1State extends State<HomeScreen> {
                                   ),
                                 ],
                               ),
-                                  
-                                  Icon(
-                                    Icons.card_giftcard,
-                                  ),
-                              
+                              Icon(
+                                Icons.card_giftcard,
+                              ),
                             ],
                           ),
                         ),
@@ -182,6 +177,18 @@ class _Screen1State extends State<HomeScreen> {
                   ],
                 ),
               ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PostPage()),
+              );
+            },
+            child: CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.black,
             ),
           ),
         ],
